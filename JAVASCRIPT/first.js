b91d5aab65ce158c0cfda42d65d80b5effeb0ce0
Asura0107 <mircoorosio@gmail.com>
2023-10-11 22:53:25 +0200

    U1-W3-D3

--- JAVASCRIPT/first.js
+++ JAVASCRIPT/first.js
@@ -12,19 +12,22 @@ window.onload = function () {
     // const newDelate = document.createElement("span");
     // newDelate.innerText = "Delate";
     // newDelate.className = "bye";
+    const divbtn3 = document.createElement("div");
+    divbtn3.className = "divbtn3";
 
     const btn3 = document.createElement("button");
     btn3.className = "classbtn3";
     btn3.innerText = "Delate";
     console.log(items);
     // items.appendChild(newDelate);
+    divbtn3.appendChild(btn3);
     items.appendChild(lispan);
-    items.appendChild(btn3);
+    items.appendChild(divbtn3);
     tasklist.appendChild(items);
 
     btn3.onclick = function (e) {
       const clickbtn = e.target;
-      clickbtn.parentNode.remove();
+      clickbtn.parentNode.parentNode.remove();
     };
   };
   addtask.value = "";
